9d0a746709358f2bbf43b5c5dc50540481c7b8eb
Ashutosh Mohanty <mohantyashu.07@gmail.com>
2024-09-16 11:36:35 +0530

    add code sort-linked-list

---
+++ src/interview_prep/dataStructures/leetcode/linkedlist/ReverseKNodes.java
@@ -0,0 +1,26 @@
+package interview_prep.dataStructures.leetcode.linkedlist;
+
+/**
+ * LC 25
+ *
+ * Given the head of a linked list, reverse the nodes of the list k at a time,
+ * and return the modified list.
+ *
+ * k is a positive integer and is less than or equal to the length of the linked list.
+ * If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+ *
+ * You may not alter the values in the list's nodes, only nodes themselves may be changed.
+ *
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [2,1,4,3,5]
+ *
+ * Input: head = [1,2,3,4,5], k = 3
+ * Output: [3,2,1,4,5]
+ */
+public class ReverseKNodes {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+
+        return head;
+    }
+}
--- src/interview_prep/dataStructures/leetcode/linkedlist/SortList.java
+++ src/interview_prep/dataStructures/leetcode/linkedlist/SortList.java
@@ -7,16 +7,17 @@ import interview_prep.dataStructures.Node;
  * Given the head of a linked list, return the list after sorting it in ascending order.
  * Given: 4->2->1->3
  * Output:1->2->3->4
+ * https://www.youtube.com/watch?v=13UkRumpqZw
  */
 public class SortList {
 
     public static void main(String[] args) {
 
         ListNode head = createList();
-        iterateNodes(head);
+        printHead(head);
         System.out.println("\n---------------");
-        new SortList().sortList(head);
-        iterateNodes(head);
+        ListNode sortedHead = new SortList().sortList(head);
+        printHead(sortedHead);
 
     }
 
@@ -26,14 +27,22 @@ public class SortList {
             return head;
         }
 
-        ListNode leftTail = findMid(head);
-        ListNode rightHead = leftTail.next;
-        leftTail.next = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = head;
+        while(fast!=null && fast.next!=null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null; //first list tail points to null
 
         ListNode head1 = sortList(head);
-        ListNode head2 = sortList(rightHead);
-        merge(head1, head2);
-        return head;
+        //printHead(head1);
+        ListNode head2 = sortList(slow);
+        //printHead(head2);
+
+        return merge(head1, head2);
     }
 
     ListNode merge(ListNode head1, ListNode head2) {
@@ -41,15 +50,11 @@ public class SortList {
         ListNode ansTail = ansHead;
 
         while (head1 != null && head2 != null) {
-            if(head1.val < head2.val) {
+            if(head1.val <= head2.val) {
                 ansTail.next = new ListNode(head1.val);
                 head1 = head1.next;
-            } else if(head1.val > head2.val) {
-                ansTail.next = new ListNode(head2.val);
-                head2 = head2.next;
             } else {
-                ansTail.next = new ListNode(head1.val);
-                head1 = head1.next;
+                ansTail.next = new ListNode(head2.val);
                 head2 = head2.next;
             }
             ansTail = ansTail.next;
@@ -67,7 +72,15 @@ public class SortList {
             head2 = head2.next;
         }
 
-        return ansTail.next;
+        ListNode t = ansHead;
+        ansHead = ansHead.next;
+        t = null;
+        return ansHead;
+    }
+
+    private static void printHead(ListNode head1) {
+        iterateNodes(head1);
+        System.out.println();
     }
 
     // 1 -> 2 -> 3 -> 4 -> 5
@@ -94,10 +107,9 @@ public class SortList {
     }
 
     static void iterateNodes(ListNode head) {
-        ListNode tempHead = head;
-        while (tempHead != null) {
-            System.out.print(tempHead.val + " ");
-            tempHead = tempHead.next;
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
         }
     }
 }
