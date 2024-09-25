db7c569a1f29366ae0172afd772ffd00095ad6ac
Ashutosh Mohanty <mohantyashu.07@gmail.com>
2024-09-26 01:19:19 +0530

    Create Majority_Element.java

---
+++ src/interview_prep/dataStructures/leetcode/array_strings/Majority_Element.java
@@ -0,0 +1,33 @@
+package interview_prep.dataStructures.leetcode.array_strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Majority_Element {
+
+    public static void main(String[] args) {
+            int[] nums = {3,2,3};
+            new Majority_Element().majorityElement(nums);
+    }
+
+    public int majorityElement(int[] nums) {
+        int L = nums.length;
+        int N = L / 2;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int i =0;
+        while(i < L) {
+            if (map.containsKey(nums[i])) {
+                int val = map.get(nums[i]);
+                if (++val > N) break;
+                map.put(nums[i], val);
+
+            } else {
+                map.put(nums[i], 1);
+            }
+            i++;
+        }
+        return nums[i];
+    }
+
+}
