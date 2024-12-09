e4c1143d2840aebd9ca41c8d43bed51d37124d45
Ashutosh Mohanty <41833078+usernameAshu@users.noreply.github.com>
2024-12-09 21:36:33 +0530

    test git changes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! running in Mac OS");
         System.out.println("Discovering new Shortcuts");
+        System.out.println("Testing mac git");
     }
 }
